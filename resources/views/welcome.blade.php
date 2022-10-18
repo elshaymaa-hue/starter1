942e8d2734ee9688f348f48161e96ae6903c26f5
shaymaa <nassarelshaymaa@gmail.com>
2022-10-18 14:29:47 +0200

    18-10-2022

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,6 +120,15 @@
                     <img class="img-fluid" src="assets/img/portfolio/submarine.png" alt="..." />
                 </div>
             </div>
+            <!-- Portfolio Item 7-->
+              <div class="col-md-6 col-lg-4">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal7">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="assets/img/portfolio/security.png" alt="..." />
+                </div>
+            </div>
         </div>
     </div>
 </section>
@@ -457,6 +466,40 @@
                             {{-- <button class="btn btn-primary" data-bs-dismiss="modal">
                                 <i class="fas fa-xmark fa-fw"></i>
                                 <a href="{{ url('offers/filter?search_=technical_office') }}">technical_office</a>
+                            </button> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio Modal 7-->
+<div class="portfolio-modal modal fade" id="portfolioModal7" tabindex="-1" aria-labelledby="portfolioModal7" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div class="modal-body text-center pb-5">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Portfolio Modal - Title-->
+                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Security Section</h2>
+                            <!-- Icon Divider-->
+                            <div class="divider-custom">
+                                <div class="divider-custom-line"></div>
+                                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                <div class="divider-custom-line"></div>
+                            </div>
+                            <!-- Portfolio Modal - Image-->
+                            <a href="{{ url('offers/filter?search_=Security') }}">
+                            <img class="img-fluid rounded mb-5" src="assets/img/portfolio/security.png" alt="..." />
+                            </a>
+                            <!-- Portfolio Modal - Text-->
+                            <p class="mb-4"></p>
+                            {{-- <button class="btn btn-primary" data-bs-dismiss="modal">
+                                <i class="fas fa-xmark fa-fw"></i>
+                                <a href="{{ url('offers/filter?search_=Electorinc+Archive') }}">software dev</a>
                             </button> --}}
                         </div>
                     </div>
