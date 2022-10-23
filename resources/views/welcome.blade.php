9a8a72665459f68b36d7a6ba9359ae648496a9c8
shaymaa <nassarelshaymaa@gmail.com>
2022-10-23 14:14:33 +0200

    23-10-2022

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,17 +120,8 @@
                     <img class="img-fluid" src="assets/img/portfolio/submarine.png" alt="..." />
                 </div>
             </div>
-            <!-- Portfolio Item 8-->
-            <div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
-                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal8">
-                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                    </div>
-                    <img class="img-fluid" src="assets/img/portfolio/tawkitat.png" alt="..." />
-                </div>
-            </div>
             <!-- Portfolio Item 7-->
-              <div class="col-md-6 col-lg-4">
+            <div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
                 <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal7">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
@@ -138,8 +129,24 @@
                     <img class="img-fluid" src="assets/img/portfolio/security.png" alt="..." />
                 </div>
             </div>
-        </div>
-    </div>
+            <!-- Portfolio Item 8-->
+            <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal8">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="assets/img/portfolio/tawkitat.png" alt="..." />
+                </div>
+            </div>
+            <!-- Portfolio Item 9-->
+            <div class="col-md-6 col-lg-4">
+                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal9">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="assets/img/portfolio/vtms.png" alt="..." />
+                </div>
+            </div>
 </section>
 <!-- About Section-->
 <section class="page-section bg-primary text-white mb-0" id="about">
@@ -537,6 +544,40 @@
                             <!-- Portfolio Modal - Image-->
                             <a href="{{ url('offers/filter?search_=Security') }}">
                             <img class="img-fluid rounded mb-5" src="assets/img/portfolio/security.png" alt="..." />
+                            </a>
+                            <!-- Portfolio Modal - Text-->
+                            <p class="mb-4"></p>
+                            {{-- <button class="btn btn-primary" data-bs-dismiss="modal">
+                                <i class="fas fa-xmark fa-fw"></i>
+                                <a href="{{ url('offers/filter?search_=Electorinc+Archive') }}">software dev</a>
+                            </button> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio Modal 9-->
+<div class="portfolio-modal modal fade" id="portfolioModal9" tabindex="-1" aria-labelledby="portfolioModal9" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div class="modal-body text-center pb-5">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Portfolio Modal - Title-->
+                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">VTMS Section</h2>
+                            <!-- Icon Divider-->
+                            <div class="divider-custom">
+                                <div class="divider-custom-line"></div>
+                                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                <div class="divider-custom-line"></div>
+                            </div>
+                            <!-- Portfolio Modal - Image-->
+                            <a href="{{ url('offers/filter?search_=vtms') }}">
+                            <img class="img-fluid rounded mb-5" src="assets/img/portfolio/vtms.png" alt="..." />
                             </a>
                             <!-- Portfolio Modal - Text-->
                             <p class="mb-4"></p>
